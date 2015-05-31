a44753f4ef12011e3fc825ecc8380fe639cb600f
lucasvmiguel <lucasvmiguel@live.com>
2015-05-31 12:13:03 -0300

    primeiro commit

---
+++ C#/TesteWebSocket/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp.Server;
+
+namespace TesteWebSocket
+{
+    class testeComportamentoWS : WebSocketBehavior
+    {
+        protected override void OnMessage(WebSocketSharp.MessageEventArgs e)
+        {
+            string msg = "ficou vazio";
+
+            switch (e.Data)
+            {
+                case "1":
+                    msg = "Dia 1";
+                    break;
+                case "2":
+                    msg = "Dia 2";
+                    break;
+                default:
+                    msg = "nenhum dia";
+                    break;
+            }
+
+            Send(msg);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wsServer = new WebSocketServer("ws://localhost:2000");
+            wsServer.AddWebSocketService<testeComportamentoWS>("/");
+            wsServer.Start();
+            Console.ReadKey();
+            wsServer.Stop();
+        }
+    }
+}
